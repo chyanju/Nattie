a5135995e54574198e0222fdb601299e063d494e
chyanju <chyanju@gmail.com>
2021-02-22 10:41:38 -0800

    make server init

--- server/app.py
+++ server/app.py
@@ -20,49 +20,34 @@ def get_uuid(query_ip):
 
 @app.route('/hello/')
 def hello():
-	return "Hello World!"
+	return "Hello World! If you see this message, it usually means the website is working and responding."
 
-@app.route('/r7')
-def render_r7():
-	request_ip = request.remote_addr
-	return render_template('r7.html', uuid=get_uuid(request_ip), uip=request_ip)
+@app.route('/')
+def render_default():
+	# request_ip = request.remote_addr
+	return render_template('default.html')
 
-@app.route('/r52')
-def render_r52():
-	request_ip = request.remote_addr
-	return render_template('r52.html', uuid=get_uuid(request_ip), uip=request_ip)
-
-@app.route('/r85')
-def render_r85():
-	request_ip = request.remote_addr
-	return render_template('r85.html', uuid=get_uuid(request_ip), uip=request_ip)
-
-@app.route('/r87')
-def render_r87():
-	request_ip = request.remote_addr
-	return render_template('r87.html', uuid=get_uuid(request_ip), uip=request_ip)
-
-@app.route('/submit', methods=["POST"])
-def process_submission():
-	request_data = request.get_json()
-	# append ip
-	request_data["ip"] = request.remote_addr
-	file_prefix = "{}_{}".format(request_data["benchmark"], get_uuid(request.remote_addr))
-	file_postfix = 0
-	while True:
-		if os.path.isfile("./submissions/{}_{}.log".format(file_prefix, file_postfix)):
-			file_postfix += 1
-		else:
-			break
-	file_to_write = "{}_{}.log".format(file_prefix, file_postfix)
-	print("# write submission to: {}".format(file_to_write))
-	# write as json
-	with open("./submissions/{}".format(file_to_write), "w") as f:
-		json.dump(request_data, f)
-	# then return
-	return make_response(
-		jsonify({'status':'successful'}), 200
-	)
+# @app.route('/submit', methods=["POST"])
+# def process_submission():
+# 	request_data = request.get_json()
+# 	# append ip
+# 	request_data["ip"] = request.remote_addr
+# 	file_prefix = "{}_{}".format(request_data["benchmark"], get_uuid(request.remote_addr))
+# 	file_postfix = 0
+# 	while True:
+# 		if os.path.isfile("./submissions/{}_{}.log".format(file_prefix, file_postfix)):
+# 			file_postfix += 1
+# 		else:
+# 			break
+# 	file_to_write = "{}_{}.log".format(file_prefix, file_postfix)
+# 	print("# write submission to: {}".format(file_to_write))
+# 	# write as json
+# 	with open("./submissions/{}".format(file_to_write), "w") as f:
+# 		json.dump(request_data, f)
+# 	# then return
+# 	return make_response(
+# 		jsonify({'status':'successful'}), 200
+# 	)
 
 if __name__ == '__main__':
 	app.run(host="0.0.0.0", port=4869)
